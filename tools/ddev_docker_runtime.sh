619700227a368d756cf8ac7cb77940ad3c4bc4ea
fh <dafanhui@163.com>
2024-10-18 23:11:12 +0800

    map seperate `/usr/local/bin` into container

--- tools/ddev_docker_runtime.sh
+++ tools/ddev_docker_runtime.sh
@@ -1,17 +1,34 @@
 #!/usr/bin/env bash
 
-######################################################
+#################################################
+#### Ensure we are in the right path. ###########
+#################################################
+if [[ 0 -eq $(echo $0 | grep -c '^/') ]]; then
+    # relative path
+    EXEC_PATH=$(dirname "`pwd`/$0")
+else
+    # absolute path
+    EXEC_PATH=$(dirname "$0")
+fi
+
+EXEC_PATH=$(echo ${EXEC_PATH} | sed 's@/\./@/@g' | sed 's@/\.*$@@')
+cd $EXEC_PATH || exit 1
+#################################################
+
+source utils.sh
+
+#################################################
 
 docker_file=$1
 dir="/tmp/NB_DOCKER_RUNTIME_BUILD_DIR_${RANDOM}_$(date +%s)"
 
-mkdir -p ${HOME}/__NB_DATA__/usr_local_bin || exit 1
-chmod -R 1777 ${HOME}/__NB_DATA__ # allow some unknown files, `|| exit 1`
+mkdir -p ${HOME}/__NB_DATA__/{usr_local_bin,tmp} || die
+chmod -R 1777 ${HOME}/__NB_DATA__ # allow some unknown files, `|| die`
 
-mkdir $dir || exit 1
-cd $dir || exit 1
-cp ~/.ssh/authorized_keys ./ || exit 1
-cp $docker_file ./Dockerfile || exit 1
+mkdir $dir || die
+cd $dir || die
+cp ~/.ssh/authorized_keys ./ || die
+cp $docker_file ./Dockerfile || die
 
 ######################################################
 
@@ -23,22 +40,23 @@ docker_image_cnt=$(docker images --format json | jq '.Tag' | grep -c 'nbnet_24.0
 podman_image_cnt=$(podman images --format json | jq '.[].Names' | grep -c 'nbnet_24.04')
 
 if [[ 0 -eq ${docker_image_cnt} && 0 -eq ${podman_image_cnt} ]]; then
-    docker pull ubuntu:24.04 || exit 1
-    docker tag ubuntu:24.04 ubuntu:nbnet_24.04 || exit 1
+    docker pull ubuntu:24.04 || die
+    docker tag ubuntu:24.04 ubuntu:nbnet_24.04 || die
 fi
 
 which docker
-docker build -t ubuntu:nbnet_runtime_v0 . || exit 1
+docker build -t ubuntu:nbnet_runtime_v0 . || die
 
 docker rm -f nbnet_runtime
 
 docker run --restart always -d --network=host \
-    -v ${HOME}/__NB_DATA__:/tmp \
+    -v ${HOME}/__NB_DATA__/tmp:/tmp \
+    -v ${HOME}/__NB_DATA__/usr_local_bin:/usr/local/bin \
     --name nbnet_runtime \
-    ubuntu:nbnet_runtime_v0 || exit 1
+    ubuntu:nbnet_runtime_v0 || die
 
 docker ps
 
-######################################################
+#################################################
 
 rm -rf $dir
